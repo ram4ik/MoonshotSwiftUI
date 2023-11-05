0cf7541e7ec9b8c150203289d7634059a74a3930
ram4ik <ram4ik@hotmail.com>
2023-11-05 21:38:26 +0200

    navigation stack

--- MoonshotSwiftUI/ContentView.swift
+++ MoonshotSwiftUI/ContentView.swift
@@ -7,28 +7,21 @@
 
 import SwiftUI
 
-struct CustomText: View {
-    let text: String
-    
-    var body: some View {
-        Text(text)
-    }
-    
-    init(text: String) {
-        print("Creating a new CustomText")
-        self.text = text
-    }
-}
+
 
 struct ContentView: View {
     var body: some View {
-        ScrollView(.horizontal) {
-            LazyHStack(spacing: 10) {
-                ForEach(0..<100) {
-                    CustomText(text: "Item \($0)")
-                        .font(.title)
+        NavigationStack {
+            NavigationLink {
+                Text("Detail View")
+            } label: {
+                HStack {
+                    Image(systemName: "swift")
+                    Text("Tap Me")
+                        .bold()
                 }
             }
+            .navigationTitle("SwiftUI")
         }
     }
 }
