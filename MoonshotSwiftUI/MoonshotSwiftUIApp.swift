f2293d56dfde223818409c0141f0cbdcd525dd79
ram4ik <ram4ik@hotmail.com>
2023-11-05 21:18:24 +0200

    Initial Commit

---
+++ MoonshotSwiftUI/MoonshotSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotSwiftUIApp.swift
+//  MoonshotSwiftUI
+//
+//  Created by admin on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
