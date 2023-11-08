9bb269e3dd5d165383c473e9428842d8eedbfc3b
ram4ik <ram4ik@hotmail.com>
2023-11-08 18:58:25 +0200

    Final views

---
+++ MoonshotSwiftUI/AstronautView.swift
@@ -0,0 +1,35 @@
+//
+//  AstronautView.swift
+//  MoonshotSwiftUI
+//
+//  Created by admin on 11/8/23.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(astronaut.description)
+                    .padding()
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    return AstronautView(astronaut: astronauts["aldrin"]!)
+        .preferredColorScheme(.dark)
+}
